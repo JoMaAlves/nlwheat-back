67c02473eb7730a9ecf6f08b1f953b6b7b6dbf6c
Joao Marcelo Alves <joao.alves@liferay.com>
2021-10-19 15:02:09 -0300

    Created login and callback routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
-import express from "express";
+import express, { request, response } from "express";
+import "dotenv/config";
 
 const app = express();
 
-app.listen(4000, () => console.log("server is running on PORT 400"));
+app.get("/github", (request,response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+})
+
+app.get("/signin/callback", (request,response) => {
+    const { code } = request.query;
+
+    return response.json(code);
+})
+
+app.listen(4000, () => console.log("server is running on PORT 4000"));
