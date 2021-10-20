5f314ada94324c35a45b22c39a605b3421aa13b8
Joao Marcelo Alves <joao.alves@liferay.com>
2021-10-19 23:36:10 -0300

    Configured websocket

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,37 @@
 import "dotenv/config";
+import http from "http";
+import cors from "cors";
 import express from "express";
 import { router } from "./routes";
+import { Server } from "socket.io";
 
 const app = express();
-app.use(express.json())
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuario conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
 
 app.use(router);
 
 app.get("/github", (request,response) => {
     response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
-})
+});
 
 app.get("/signin/callback", (request,response) => {
     const { code } = request.query;
 
     return response.json(code);
-})
+});
 
-app.listen(4000, () => console.log("server is running on PORT 4000"));
+export { serverHttp, io };
