42d053e7a55f4179457b9c277e8a942de85e81f4
Joao Marcelo Alves <joao.alves@liferay.com>
2021-10-19 23:08:28 -0300

    Saving messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { CreateMessageController } from "./controllers/CreateMessageController";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
 
 export { router }
